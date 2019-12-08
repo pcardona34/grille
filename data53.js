627442cf55d89052bf4088ff641366306c1a20e3
pcardona34 <pcardona34@gmail.com>
2019-12-08 16:29:00 +0100

    ajout d'exemple et du fichier index.

---
+++ data53.js
@@ -0,0 +1,22 @@
+var oygCrosswordPuzzle = new oyCrosswordPuzzle (
+"3903567316",
+"./JCE",
+"/a/a",
+"Mots croisés",
+"Lexique de la dictée 53",
+[
+new oyCrosswordClue(10,"Adj. Qui est rempli par des créatures qui s'agitent dans tous les sens.","GROUILLANT","43819436c48ade948ca0062d4be70c86",1,11,1)
+,new oyCrosswordClue(9,"Adj. Dans lequel on dépose un mort ou ses cendres.","FUNERAIRE","a788b3c0f65ceb0f713e7332b2489806",1,3,2)
+,new oyCrosswordClue(6,"Qui a mauvaise réputation.","INFAME","4926fd2934d2e1613d8273320bbf51f5",1,7,7)
+,new oyCrosswordClue(8,"Ce qui est construit ou dressé comme un lieu de mémoire.","MONUMENT","4d76eb79a002fc247b0d56219355f389",0,10,3)
+,new oyCrosswordClue(7,"Souffrir, attendre sa libération.","LANGUIR","4a9eb3cbf9fd194fe0c23852da49882d",0,8,1)
+,new oyCrosswordClue(8,"Adj. Collant comme de la glu.","VISQUEUX","7de3440b117624a0bd284b375ac63c1c",0,10,5)
+,new oyCrosswordClue(10,"Adj. Digne d'être haï, odieux.","ABOMINABLE","1e03b5f48559e7a8115b609b1d18d894",0,3,7)
+,new oyCrosswordClue(8,"Un être vivant.","CREATURE","a9889995e0732b01a8f6693417f1bf47",0,1,5)
+,new oyCrosswordClue(6,"Apparaître brusquement en se dressant devant l'observateur.","SURGIR","f01a61b6eab5b2eec609a22ab8ff00b3",0,2,3)
+,new oyCrosswordClue(6,"Tour la plus imposante d'un château.","DONJON","2d96cd0bdffb425a75682b86ad15624b",0,9,9)
+],
+20,20);
+oygCrosswordPuzzle.publisherName = "Patrick CARDONA";
+oygCrosswordPuzzle.leaveGameURL = "./licence.html";
+oygCrosswordPuzzle.canTalkToServer = false;
