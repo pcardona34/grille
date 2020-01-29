e404bae98d098b0df87dd5062c3d695672aa3388
Patrick Cardona <pcardona34@gmail.com>
2020-01-29 11:49:18 +0100

    Données déplacées dans le dossier: data

---
+++ data/data_le_cor.js
@@ -0,0 +1,21 @@
+var oygCrosswordPuzzle = new oyCrosswordPuzzle (
+"8254130576",
+"./JCE",
+"/a/a",
+"Mots croisés",
+"Le lexique dans « le cor » (extrait)",
+[
+new oyCrosswordClue(7,"Laisser échapper de la bouche, en parlant d'un son ou d'un souffle.","EXHALER","1d90f98c0e22e8e296ec7dcd222c8706",1,6,6)
+,new oyCrosswordClue(9,"Col des Pyrénées.","RONCEVAUX","f65e1a3c5ade69a372ca7fb1709cca5e",1,2,5)
+,new oyCrosswordClue(7,"Chevalier du temps de Charlemagne.","PALADIN","74c1f5ae1b84dd9a0c8e3f77d07c0486",1,9,4)
+,new oyCrosswordClue(11,"Qui annonce les évènements à venir.","PROPHETIQUE","322bb8339c2b4c1e0ce82700921a8376",1,11,0)
+,new oyCrosswordClue(5,"Quand le gibier est cerné par la meute.","ABOIS","2aa47665b56ef357e60e009015411ea9",1,14,3)
+,new oyCrosswordClue(5,"Fantôme.","OMBRE","2285afe47810b4e1d3c04d180a7f7a5f",0,2,6)
+,new oyCrosswordClue(7,"Ancien.","ANTIQUE","63309542addd7c57fc474db5698207d6",0,6,9)
+,new oyCrosswordClue(4,"Entendre (vieilli).","OUIR","3ade517c6820142c4c26c30983be8017",0,8,1)
+,new oyCrosswordClue(4,"Son répété.","ECHO","a52ae19f4507791c1883c6d6ce3980c6",0,11,5)
+],
+20,20);
+oygCrosswordPuzzle.publisherName = "Patrick Cardona";
+oygCrosswordPuzzle.leaveGameURL = "./licence.html";
+oygCrosswordPuzzle.canTalkToServer = false;
