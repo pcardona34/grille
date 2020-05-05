fa6e13e12c307b39a96ccfe5a47627082f6efa85
Patrick Cardona <pcardona34@gmail.com>
2020-05-05 09:41:08 +0200

    ajout de nouvelles grilles

---
+++ data/data54.js
@@ -0,0 +1,19 @@
+var oygCrosswordPuzzle = new oyCrosswordPuzzle (
+"1876450204",
+"./JCE",
+"/a/a",
+"Mots croisés",
+"Le lexique de la dictée 54",
+[
+new oyCrosswordClue(10,"Sans être puni, sans subir les conséquences de ses actes.","IMPUNEMENT","e7f193a1e944975b0020917644dfa509",1,5,1)
+,new oyCrosswordClue(8,"User librement de quelque chose.","DISPOSER","d8002142df2653ca9bc57524a156a85f",1,8,2)
+,new oyCrosswordClue(4,"Détester.","HAIR","ebd9cbc1d3a3d80df95594b4e6e20655",1,13,1)
+,new oyCrosswordClue(5,"Souci d'aider quelqu'un, intérêt qu'on lui manifeste (au pluriel).","SOINS","5a2ede2035e63d2cf6f97ccbcc08c9b5",0,2,9)
+,new oyCrosswordClue(5,"Danger.","PERIL","0a42a86d73d17246528a11a8446cd5d4",0,2,1)
+,new oyCrosswordClue(11,"Craintif, timide.","PUSILLANIME","0fa17c6433a73aa46c43dd992c795056",0,5,3)
+],
+20,20);
+oygCrosswordPuzzle.publisherName = "Patrick Cardona";
+oygCrosswordPuzzle.publisherURL = "http://patrick.cardona.free.fr/";
+oygCrosswordPuzzle.leaveGameURL = "./apropos.html";
+oygCrosswordPuzzle.canTalkToServer = false;
---
+++ data/data_les_conquerants.js
@@ -0,0 +1,26 @@
+var oygCrosswordPuzzle = new oyCrosswordPuzzle (
+"4504711030",
+"./JCE",
+"/a/a",
+"Mots croisés",
+"Le lexique dans le poème « Les conquérants »",
+[
+new oyCrosswordClue(9,"v. Envoûter, jeter un sort.","ENCHANTER","3f437dfb6c572566cbb1bd524c51da05",1,20,2)
+,new oyCrosswordClue(6,"n.f. Pauvreté.","MISERE","648eba4951b133ab1b057c269e9f6a1e",1,22,3)
+,new oyCrosswordClue(9,"n. f. Navire de haute mer à la Renaissance.","CARAVELLE","1ca879622bf6dde4e0e7f9e7d627b37a",1,14,1)
+,new oyCrosswordClue(7,"adj. Fier, méprisant.","HAUTAIN","9b79a3ab3493fdfbbce4d160a5c51d25",1,4,6)
+,new oyCrosswordClue(5,"adj. Du lieu de naissance.","NATAL","ce93e354999cbc491fede9e880b3ac9a",1,18,2)
+,new oyCrosswordClue(8,"Lieu où sont déposés des cadavres ou des ossements.","CHARNIER","e57f15e2ece9ef61a3e6c1d7e3c46865",1,8,5)
+,new oyCrosswordClue(7,"n.m. Objet ou animal tué à la chasse, qu'on rapporte en triomphe.","TROPHEE","5ed377e553de583ccfa428819584325d",1,17,7)
+,new oyCrosswordClue(7,"n.f. Vergue qui s'attache obliquement au mât.","ANTENNE","f6bf94d0a927107efa656d068edf6e7c",0,14,2)
+,new oyCrosswordClue(7,"n.m. Soldat sans morale, mercenaire.","ROUTIER","4ee3be727c1ee0e68158f094a439c8bd",0,17,8)
+,new oyCrosswordClue(6,"n.m. Illusion d'optique et rêve au sens figuré.","MIRAGE","eb3098dfeb8e122bb77b505eef0f8164",0,11,4)
+,new oyCrosswordClue(14,"adj. Qui émet de la lumière.","PHOSPHORESCENT","622be24738c015f248c0cb1b8ab755d6",0,3,6)
+,new oyCrosswordClue(5,"adj. Se dit d'un vent de force modérée.","ALIZE","07d2b0483384c1c28c3666ecc92e315b",0,10,9)
+,new oyCrosswordClue(7,"n.m. Grand rapace de la famille des faucons.","GERFAUT","c0b6130df9b32945d81e28a325d2e4ae",0,7,11)
+,new oyCrosswordClue(9,"v. S'emparer par la force.","CONQUERIR","71d2aae4fabc2a46e21444fc9bf9fc3c",0,12,13)
+],
+25,25);
+oygCrosswordPuzzle.publisherName = "Patrick Cardona";
+oygCrosswordPuzzle.leaveGameURL = "./licence.html";
+oygCrosswordPuzzle.canTalkToServer = false;
