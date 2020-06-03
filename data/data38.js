949f3930f097df280547e7bc969891acea51a562
Patrick Cardona <pcardona34@gmail.com>
2020-06-03 13:12:11 +0200

    Ajout d'une nouvelle grille

---
+++ data/data38.js
@@ -0,0 +1,24 @@
+var oygCrosswordPuzzle = new oyCrosswordPuzzle (
+"3916011316",
+"./JCE",
+"/a/a",
+"Mots croisés",
+"Lexique de la dictée 38",
+[
+new oyCrosswordClue(10,"Orner de rubans.","ENRUBANNER","f6f5a2e4a8bb8e4fe1482fcb27139ffc",1,8,0)
+,new oyCrosswordClue(9,"Illustrer un manuscrit, orner de couleurs vives.","ENLUMINER","7877021443212d29e7f65eeebce3f6b4",1,4,3)
+,new oyCrosswordClue(9,"Ornement et devise  qu'on plaçait autrefois sur les écus.","ARMOIRIES","c6af30b92d00f9b2ec0c761d742630bd",1,11,0)
+,new oyCrosswordClue(8,"Pauvre.","INDIGENT","dcb27930d1d211c5c68bbdd9835b27bf",1,13,0)
+,new oyCrosswordClue(10,"Diviser une surface en carrés ou en losanges.","QUADRILLER","d60d4eb5dcf0ce167ff7bd5de6a90a8b",1,1,1)
+,new oyCrosswordClue(7,"Ancien nom du pays basque.","BISCAYE","c66130e4f9e7c0145f672d254d243ab3",1,2,11)
+,new oyCrosswordClue(7,"Bateau à fond plat.","HOURQUE","8dd188740755e1817cee31bcc44b3f08",1,6,10)
+,new oyCrosswordClue(8,"Orner de galons.","GALONNER","6b1553cac44811fe6947c758cb926d05",0,4,1)
+,new oyCrosswordClue(7,"Travailler une matière avec un ciseau.","CISELER","ff78e3b2ad94c5912a6bad7333251c3d",0,3,8)
+,new oyCrosswordClue(9,"Orner de dorures ou de couleurs vives.","CHAMARRER","fa170d1fdb6430565f0bbf89c94cd88e",0,6,5)
+,new oyCrosswordClue(4,"Qui donne une impression de rudesse.","APRE","d0b77c25bc529e2505a8b6cb75dc4540",0,1,3)
+,new oyCrosswordClue(9,"Peinture au moyen de couleurs vives et variées.","BARIOLAGE","2b25ef1a2c1da0057ce1288a8d670349",0,2,11)
+],
+20,20);
+oygCrosswordPuzzle.publisherName = "Patrick CARDONA";
+oygCrosswordPuzzle.leaveGameURL = "./licence.html";
+oygCrosswordPuzzle.canTalkToServer = false;
