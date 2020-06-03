804018fd1c4af37717b8f3f6310f0ecff8628e26
Patrick Cardona <pcardona34@gmail.com>
2020-06-03 23:20:55 +0200

    Ajout d'une nouvelle grille

---
+++ data/data_inconnu.js
@@ -0,0 +1,20 @@
+var oygCrosswordPuzzle = new oyCrosswordPuzzle (
+"1455774328",
+"./JCE",
+"/a/a",
+"Mots croisés",
+"Inconnu à cette adresse",
+[
+new oyCrosswordClue(8,"Qualité de l'affaire des associés.","PROSPERE","7ac8e2050d3bc7a8b444c0b6c52f1d3b",1,3,3)
+,new oyCrosswordClue(4,"Parti du nouveau chancelier.","NAZI","77f0d89a25fca0cc0484ef4583a859b1",1,5,1)
+,new oyCrosswordClue(4,"Dort dans un lit monumental.","ELSA","85e3558660413f4f2136ec01ddf8f05a",1,9,4)
+,new oyCrosswordClue(10,"Haineux envers les juifs.","ANTISEMITE","bc3de18d45f5136806c088944f9a92e1",1,12,2)
+,new oyCrosswordClue(6,"Où Griselle devait triompher après Vienne.","BERLIN","d40d7673b41f9d90e2bb91d3421f9ad7",0,1,4)
+,new oyCrosswordClue(6,"Premier camp de concentration en Allemagne.","DACHAU","ef481f88b93c8fa4aaa477d30660d59d",0,4,2)
+,new oyCrosswordClue(10,"Indice de luxe dans la nouvelle maison de Martin.","PORCELAINE","2e5fba7522159670379c9fba693dc5b5",0,3,7)
+,new oyCrosswordClue(7,"Massacre de juifs.","POGROME","8bff497ca4c5c710e44aeb555b31f4c7",0,6,11)
+],
+20,20);
+oygCrosswordPuzzle.publisherName = "Patrick CARDONA";
+oygCrosswordPuzzle.leaveGameURL = "./licence.html";
+oygCrosswordPuzzle.canTalkToServer = false;
