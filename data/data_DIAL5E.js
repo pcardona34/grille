697b4b3c03e78028852bce105ca604e934fe92ad
Patrick Cardona <pcardona34@gmail.com>
2020-06-05 01:08:39 +0200

    Ajout d'une nouvelle grille

---
+++ data/data_DIAL5E.js
@@ -0,0 +1,21 @@
+var oygCrosswordPuzzle = new oyCrosswordPuzzle (
+"5245276345",
+"./JCE",
+"/a/a",
+"Mots croisés",
+"Le dialogue",
+[
+new oyCrosswordClue(8,"Tiret large.","CADRATIN","12b65cdc8fb82b2698c08d9e780397e4",1,12,3)
+,new oyCrosswordClue(8,"Un échange de répliques.","DIALOGUE","cf84bc9c319b158411c61b7f0d573bc3",1,9,1)
+,new oyCrosswordClue(7,"Se dit du sujet placé après le verbe.","INVERSE","4248907c58bf1d56394eb9901d25918f",1,6,8)
+,new oyCrosswordClue(9,"Discours pour soi-même.","MONOLOGUE","0d35f3e63f0070128d030e5983a84714",1,4,2)
+,new oyCrosswordClue(11,"Une prise de parole.","DECLARATION","804a42682bd80a8367d7420fc641adba",1,1,8)
+,new oyCrosswordClue(10,"Ces signes encadrent un réplique isolée ou un bloc de dialogue.","GUILLEMETS","bef4c7d0f5ab39a1907cccece74bf5dd",0,4,8)
+,new oyCrosswordClue(6,"Cette proposition nous indique le locuteur.","INCISE","add97e3ae680700c0b6bc0faea35dcc4",0,6,2)
+,new oyCrosswordClue(10,"Attire l'attention de l'interlocuteur.","APOSTROPHE","dbcbb6e71a1fc60184eb98ceb3d1c9e1",0,1,12)
+,new oyCrosswordClue(10,"Marque l'intention de celui qui parle.","INTONATION","decbdcbcf9f0cb0de97b7e9f69ab5e1f",0,1,5)
+],
+20,20);
+oygCrosswordPuzzle.publisherName = "Patrick CARDONA";
+oygCrosswordPuzzle.leaveGameURL = "./licence.html";
+oygCrosswordPuzzle.canTalkToServer = false;
